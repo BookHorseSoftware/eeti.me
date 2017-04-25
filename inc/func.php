98763e905ae9d3a7b506f10938fd21b1ab1d9b1b
Alexandra Frock <im@alexandra.moe>
2017-04-25 17:20:02 -0400

    Now why was that a thing

--- inc/func.php
+++ inc/func.php
@@ -88,4 +88,13 @@
 
     return $nuid;
   }
+
+    function initUnknowns($userprofile){
+      if( ! @isset($userprofile->realname) ) $userprofile->realname="";
+      if( ! @isset($userprofile->bio) ) $userprofile->bio="";
+      if( ! @isset($userprofile->homepage) ) $userprofile->homepage="";
+      if( ! @isset($userprofile->avatar) ) $userprofile->avatar="";
+      return $userprofile;
+    }
+
 ?>
